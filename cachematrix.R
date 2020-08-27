1eab1fab68bad100d20972acade1096e3d5c59e1
JoanCamiloMina <joan.c.mina95@gmail.com>
2020-08-26 20:23:39 -0500

    Solution to ProgrammingAssignment #3 Using Cache to store the calculation of the inverse of a matrix, using the concept of a constructive function that generates a list of function to access a set through cache the inverse of the matrix

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,54 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+"makeCacheMatrix, crea una matriz que puede almacena en cache su inversa"
+#x es por defecto una matriz de 3 x 3 con valores de una distribución normal
+makeCacheMatrix <- function(x = matrix(rnorm(9), nrow=3, ncol=3)){
+  i <- NULL
+  
+  set <- function(y){
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  set_inverse <- function(inverse) i <<- inverse
+  get_inverse <- function() i
+  
+  list(set=set, get=get, 
+       set_inverse = set_inverse,
+       get_inverse = get_inverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+"cacheSolve, calcula la inversa de la matriz generada con la función 
+makeCacheMatrix, sí encuentra un valor en la cache lo retorna en lugar
+de calcular."
+#x es la lista con las funciones generadas por makeCacheMatrix
+cacheSolve <- function(x, ...){
+  i<-x$get_inverse()
+  'Valida sí hay la inversa está en cache'
+  if(!is.null(i)){
+    message("getting cache data")
+    return (i)
+  }
+  'sí la inversa no está en la cache la calcula y la guarda en cache'
+  matrix <- x$get()
+  i <- solve(matrix)
+  x$set_inverse(i)
+  i
 }
+
+useMyCacheInverse <- function(){
+  makematrixfunc <- makeCacheMatrix()
+  
+  message("Original Matrix")
+  print(makematrixfunc$get())
+  
+  message("Inverse Matrix without cache")
+  print(cacheSolve(makematrixfunc))
+  
+  message("Getting inverse from cache")
+  print(cacheSolve(makematrixfunc))
+  
+}
+
+useMyCacheInverse()
